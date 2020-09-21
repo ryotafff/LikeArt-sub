97ce2214e6f5d277a0c40ff3b033b8dd26153e8f
ryotafff <jnblog2019@gmail.com>
2020-09-21 08:44:47 +0000

    Update Ajax attachment-image in Artwork

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -43,3 +43,16 @@ $(document).on('turbolinks:load', function () {
     hidePrevious : false
   });
 });
+
+
+$(document).on('turbolinks:load', function () {
+    $(function(){
+        $('#artwork-image').on('change', function(e) {
+            var reader = new FileReader();
+            reader.onload = function (e) {
+                $(".image").attr('src', e.target.result);
+            }
+            reader.readAsDataURL(e.target.files[0]);
+        });
+    })
+});
