e8c58354e0e4f94bab32f43fdf4312b5713a2a20
ryotafff <jnblog2019@gmail.com>
2020-10-13 08:54:12 +0000

    Fix Readable Code

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -10,12 +10,10 @@ class UsersController < ApplicationController
 
 
   def edit
-    @user = User.find(params[:id])
   end
 
 
   def update
-    @user = User.find(params[:id])
     if @user.update(user_params)
       redirect_to user_path(@user), notice: "会員情報を更新しました"
     else
